21b5e321f7b11e8f80f69a429b7d4307ec628cb9
Fernando Campo <fernando.campo@telefonica.com>
2014-03-28 15:30:17 +0000

    Added time control for test

--- apps/communications/ftu/js/silentSms.js
+++ apps/communications/ftu/js/silentSms.js
@@ -8,7 +8,7 @@ var SilentSMS = {
         messageId;
 
     if (!sms) {
-      console.log('> TEF > SMS not found in System. We are done here');
+      console.log('> SILENT SMS > SMS not found in System. We are done here');
       return;
     }
 
@@ -18,21 +18,32 @@ var SilentSMS = {
     // Listen for sending event to keep the sms id for later deletion
     sms.addEventListener('sending', function sending(e) {
       console.log('> SILENT SMS > sending');
+      console.log('> SILENT SMS > Time is ' + (Date.now() - start) + ' ms');
       // delete the sent message so user don't see it
+
       sms.delete(e.message.id);
       console.log('> SILENT SMS > deleting');
       // return to keep the FTE flow
+
+      // Checking how much time it took
+
+      console.log('> SILENT SMS > Final time: ' + (Date.now() - start) + ' ms');
       callback();
     });
-    console.log('> SILENT SMS > here we go');
+
+    console.log('> SILENT SMS > sending sms');
+    // Starting a timer
+    var start = Date.now();
     // Send a message
     var sendSmsRequest = sms.send(number, text);
 
     sendSmsRequest.onSuccess = function() {
       console.log('> SILENT SMS > sending SUCCESS');
+      console.log('> SILENT SMS > Time is ' + (Date.now() - start) + ' ms');
     };
     sendSmsRequest.onError = function() {
       console.log('> SILENT SMS > sending ERROR');
+      console.log('> SILENT SMS > Time is ' + (Date.now() - start) + ' ms');
     };
   }
 };
