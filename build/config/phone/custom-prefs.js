e867752c02a3c3b4841f910b8e0b1be762d10a5e
crdlc <crdlc@tid.es>
2015-03-09 10:48:19 +0100

    We have to disable CSP to load web components temporarily

--- build/config/phone/custom-prefs.js
+++ build/config/phone/custom-prefs.js
@@ -2,3 +2,4 @@
 user_pref('devtools.responsiveUI.customWidth', 320);
 user_pref('devtools.responsiveUI.customHeight', 480);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
+user_pref('security.csp.enable', false);
