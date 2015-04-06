1733afcc65b431781abd0ded895a4dc321822ccf
crdlc <crdlc@tid.es>
2015-04-06 09:50:39 +0200

    added comment with current date to modify the worker v2

--- shared/js/offliner/offliner-fetcher-urls.js
+++ shared/js/offliner/offliner-fetcher-urls.js
@@ -20,25 +20,17 @@ self.off.fetchers.urls = {
   // prefetching is done.
   prefetch: function (resources, cache) {
     return Promise.all(resources.map(function (resource) {
-      return cache.match(new Request(resource.url)).then(response => {
-        console.log(response && response.status);
-        if (response && response.status === '200') {
-          console.log('Offliner fetcher >', resource.url,  'already in cache');
-          return Promise.resolve();
-        } else {
-          // The HTTP cache is still working when leaving the service worker via
-          // `fetch()` so it's neccessary to bust the URL to recover a fresh copy.
-          var bustedUrl = resource.url + '?__b=' + Date.now();
+      // The HTTP cache is still working when leaving the service worker via
+      // `fetch()` so it's neccessary to bust the URL to recover a fresh copy.
+      var bustedUrl = resource.url + '?__b=' + Date.now();
 
-          // The request is for the busted url in no-cors mode to allow resources
-          // from other origins.
-          var request = new Request(bustedUrl, { mode: 'no-cors' });
+      // The request is for the busted url in no-cors mode to allow resources
+      // from other origins.
+      var request = new Request(bustedUrl, { mode: 'no-cors' });
 
-          console.log('Offliner fetcher >', resource.url);
-          // But when caching, the cache is for the original URL.
-          return fetch(request).then(cache.put.bind(cache, resource.url));
-        }
-      });
+      console.log('Offliner prefetching >', resource.url);
+      // But when caching, the cache is for the original URL.
+      return fetch(request).then(cache.put.bind(cache, resource.url));
     }));
   }
 };
