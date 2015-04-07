ba5df3c2aaba02e1e51c974c01e471c4110fdc07
crdlc <crdlc@tid.es>
2015-04-07 11:17:53 +0200

    added prefs for sw

--- build/config/phone/custom-prefs.js
+++ build/config/phone/custom-prefs.js
@@ -3,3 +3,6 @@ user_pref('devtools.responsiveUI.customWidth', 320);
 user_pref('devtools.responsiveUI.customHeight', 480);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
 user_pref('security.csp.enable', false);
+user_pref('network.disable.ipc.security', true);
+user_pref('dom.caches.enabled', true);
+user_pref('dom.serviceWorkers.enabled', true);
