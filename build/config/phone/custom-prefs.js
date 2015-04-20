053b96b7b09b0cce00f05cc0aeab7529c10c94d0
crdlc <crdlc@tid.es>
2015-04-20 12:28:48 +0200

    type certifed

--- build/config/phone/custom-prefs.js
+++ build/config/phone/custom-prefs.js
@@ -6,3 +6,5 @@ user_pref('security.csp.enable', false);
 user_pref('network.disable.ipc.security', true);
 user_pref('dom.caches.enabled', true);
 user_pref('dom.serviceWorkers.enabled', true);
+
+user_pref('dom.apps.developer_mode', true);
