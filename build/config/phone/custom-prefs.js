8b0e9301dbccc649492ac3d9aae93077c7362425
crdlc <crdlc@tid.es>
2015-04-20 15:35:00 +0200

    added dom.webcomponents.enabled and dom.apps.developer_mode

--- build/config/phone/custom-prefs.js
+++ build/config/phone/custom-prefs.js
@@ -2,9 +2,13 @@
 user_pref('devtools.responsiveUI.customWidth', 320);
 user_pref('devtools.responsiveUI.customHeight', 480);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
+
 user_pref('security.csp.enable', false);
 user_pref('network.disable.ipc.security', true);
 user_pref('dom.caches.enabled', true);
 user_pref('dom.serviceWorkers.enabled', true);
 
 user_pref('dom.apps.developer_mode', true);
+user_pref('dom.webcomponents.enabled', true);
+pref('dom.apps.developer_mode', true);
+pref('dom.webcomponents.enabled', true);
